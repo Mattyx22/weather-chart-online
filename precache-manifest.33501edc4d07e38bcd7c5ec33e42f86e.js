53c3e7ac2e116f778d72bd6fe13ab3a69bfda37a
Mattyx <mattyx22@gmail.com>
2019-07-11 14:09:34 +0200

    Add files via upload

---
+++ precache-manifest.33501edc4d07e38bcd7c5ec33e42f86e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c11d7fef522e0e499a12",
+    "url": "/weather-chart-online/static/js/runtime~main.c11d7fef.js"
+  },
+  {
+    "revision": "8010d6e13194a74d1502",
+    "url": "/weather-chart-online/static/js/main.8010d6e1.chunk.js"
+  },
+  {
+    "revision": "964590acb88ee371326a",
+    "url": "/weather-chart-online/static/js/2.964590ac.chunk.js"
+  },
+  {
+    "revision": "4d6f5d9d3af1466adf26ad53b9fdc32b",
+    "url": "/weather-chart-online/index.html"
+  }
+];
